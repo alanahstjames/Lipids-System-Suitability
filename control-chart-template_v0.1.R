f8bba0e11ca3454a7e2bab0e929239e2450fb495
Alanah Grant-St James <alanahstjames@outlook.com>
2023-03-16 14:17:51 +0800

    Change figure size for different versions (more standards shown in v2)

--- control-chart-template_v0.1.R
+++ control-chart-template_v0.1.R
@@ -6,39 +6,83 @@
 #' ---
 #'   
 #' ### Project Summary
-knitr::opts_chunk$set(
-  echo = FALSE,
-  fig.height = 25,
-  fig.width = 16, 
-  fig.align = 'center' 
-)
+if(master_list$project_details$is_ver == "v1") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 25,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
+
+if(master_list$project_details$is_ver == "v2") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 32,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
 #' 
 knitr::kable(master_list$summary_tables$project_summary)
 #' 
 #' 
 #' ### SIL ISTD Peak Areas
-knitr::opts_chunk$set(
-  echo = FALSE,
-  fig.height = 25,
-  fig.width = 16, 
-  fig.align = 'center' 
-)
+if(master_list$project_details$is_ver == "v1") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 25,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
+
+if(master_list$project_details$is_ver == "v2") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 32,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
 fig_ISTD
 #' 
 #' ### Analogue Analyte Peak Areas
-knitr::opts_chunk$set(
-  echo = FALSE,
-  fig.height = 25,
-  fig.width = 16, 
-  fig.align = 'center' 
-)
+if(master_list$project_details$is_ver == "v1") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 25,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
+
+if(master_list$project_details$is_ver == "v2") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 32,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
 figAnalytes
 #' 
 #' ### Response Ratios
-knitr::opts_chunk$set(
-  echo = FALSE,
-  fig.height = 25,
-  fig.width = 16, 
-  fig.align = 'center' 
-)
+if(master_list$project_details$is_ver == "v1") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 25,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
+
+if(master_list$project_details$is_ver == "v2") {
+  knitr::opts_chunk$set(
+    echo = FALSE,
+    fig.height = 32,
+    fig.width = 16, 
+    fig.align = 'center' 
+  )
+}
 figResponseRatio
